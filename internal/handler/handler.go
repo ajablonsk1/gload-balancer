5463de380f3c045bfbfb2034cfd01f2644d02fff
Adrian Jabłoński <ajablonsky@Adrians-MacBook-Pro.local>
2024-02-06 22:57:38 +0100

    Added ip hash load balancing method

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -12,6 +12,9 @@ type ProxyHandler struct {
 }
 
 func (h ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if ipHash, ok := h.Strategy.(*model.IPHash); ok {
+		ipHash.CurrSourceAddress = r.RemoteAddr
+	}
 	server := h.Strategy.GetServer(h.ServerPool)
 	server.Proxy.ServeHTTP(w, r)
 }
